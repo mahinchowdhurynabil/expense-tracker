8e348c1833d27989d151a5b5d0ca4239deab5168
mahinchowdhurynabil <mahinnabil13@gmail.com>
2026-03-07 23:44:50 +0600

    bar and doughnut charts add in the deshboard

---
+++ script.js
@@ -0,0 +1,77 @@
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Salary", "Cash in hand", "Last Month Balance"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 205, 86)",
+          "rgb(255, 99, 132)",
+          "rgb(22%, 37%, 59%)",
+        ],
+        borderRadius: 10,
+        hoverOffset: 4,
+      },
+    ],
+  },
+
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    cutout: "75%",
+    plugins: {
+      legend: {
+        position: "right", // 👈 moves labels to the right
+        labels: {
+          usePointStyle: true,
+          pointStyle: "rectRounded",
+          boxWidth: 12,
+          padding: 20,
+        },
+      },
+    },
+  },
+});
+
+const ctxBar = document.getElementById("myBarChart");
+
+new Chart(ctxBar, {
+  type: "bar",
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May"],
+
+    datasets: [
+      {
+        label: "Income",
+        data: [500, 600, 550, 700, 650],
+        backgroundColor: "rgb(54, 162, 235)",
+      },
+      {
+        label: "Expense",
+        data: [300, 400, 350, 450, 420],
+        backgroundColor: "rgb(255, 99, 132)",
+      },
+    ],
+  },
+
+  options: {
+    responsive: true,
+
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+    },
+
+    scales: {
+      x: {
+        categoryPercentage: 0.6,
+        barPercentage: 0.5,
+      },
+    },
+  },
+});
